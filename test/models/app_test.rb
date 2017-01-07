70a7d6222fc6d78e30f450228efa04276d4c72a0
Aidi Stan <aidistan@live.com>
2017-01-08 00:06:33 +0800

    Write tests for App model

--- test/models/app_test.rb
+++ test/models/app_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class AppTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @app = apps(:test)
+  end
+
+  test 'fixtures should be valid' do
+    App.all.each do |app|
+      assert app.valid?
+    end
+  end
+
+  test 'name should be present' do
+    @app.name = ' '
+    assert_not @app.valid?
+  end
+
+  test 'name should not be too long' do
+    @app.name = 'a' * 256
+    assert_not @app.valid?
+  end
+
+  test 'redirect_uri should be present' do
+    @app.redirect_uri = ' '
+    assert_not @app.valid?
+  end
+
+  test 'redirect_uri should be a valid uri' do
+    @app.redirect_uri = '0://'
+    assert_not @app.valid?
+  end
 end
