99b6e7b4b67d5b1df6d727ab049f1ef0165a5028
Aidi Stan <aidistan@live.com>
2017-02-22 02:24:37 +0800

    Bump to v0.4.0
    - Add more controllers
    - Add more policies

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 module QingDiet
   # Current version
-  VERSION = '0.3.0'.freeze
+  VERSION = '0.4.0'.freeze
   VERNAME = 'Onion'.freeze
-  VERTIME = '2017-02-09'.freeze
+  VERTIME = '2017-02-22'.freeze
 end
